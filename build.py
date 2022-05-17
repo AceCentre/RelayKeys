12d3eadca2cea53561a5a3979562aae8b4b6cd7c
will wade <willwade@gmail.com>
2022-05-17 22:31:20 +0100

    updating to deal with cli_keymaps

--- build.py
+++ build.py
@@ -1,7 +1,12 @@
 # To build you need to install NSIS, And the Simple Service plugin: https://nsis.sourceforge.io/NSIS_Simple_Service_Plugin
 # You also need pyinstaller installed
 
-import os, glob, subprocess
+import os
+import glob
+import subprocess
+import errno
+import shutil
+
 
 def moveTree(sourceRoot, destRoot):
     if not os.path.exists(destRoot):
@@ -26,41 +31,42 @@ def moveTree(sourceRoot, destRoot):
             os.rmdir(path)
     return ok
 
-# Copy the example relaykeys example. This is hacky. 
+
+# Copy the example relaykeys example. This is hacky.
 with open("relaykeys-example.cfg", "r") as f:
     with open("relaykeys.cfg", "w") as wf:
         wf.write(f.read())
 
 # Build the spec files
-for script, exename, console in [ \
+for script, exename, console in [
     ('relaykeysd.py', 'relaykeysd', True),
     ('relaykeysd-service.py', 'relaykeysd-service', True),
     ('relaykeys-cli.py', 'relaykeys-cli', True),
     ('relaykeys-cli.py', 'relaykeys-cli-win', False),
-    ('relaykeys-qt.py', 'relaykeys-qt', False) ]:
+        ('relaykeys-qt.py', 'relaykeys-qt', False)]:
   with open("relaykeys.spec.ini", "rb") as inf:
     data = str(inf.read(), "utf8").format(COL_NAME=exename,
-                                   CONSOLE="True" if console else "False",
-                                   SCRIPT=script,
-                                   EXE_NAME=exename)
+                                          CONSOLE="True" if console else "False",
+                                          SCRIPT=script,
+                                          EXE_NAME=exename)
     with open("{}.spec".format(exename), "w") as wf:
       wf.write(data)
 
 # first clear out the dist folder..
 #os.system("rd /s /q dist")
 #os.system("md ./dist")
-       
+
 # Do a pyinstaller on these files
-for spec in ['relaykeysd.spec','relaykeys-cli.spec','relaykeys-cli-win.spec','relaykeys-qt.spec']:
-    subprocess.run(["pyinstaller",spec])
+for spec in ['relaykeysd.spec', 'relaykeys-cli.spec', 'relaykeys-cli-win.spec', 'relaykeys-qt.spec']:
+    subprocess.run(["pyinstaller", '-y', spec])
 
 if os.name == 'nt':
-    subprocess.run(["pyinstaller",'relaykeysd-service.spec'])
+    subprocess.run(["pyinstaller", '-y', 'relaykeysd-service.spec'])
 
 # Create a PDF of the readme - and in future any other docs..
 # NB: I realise the next lines are insane. I'm in a hurry
 for doc in ['README.md']:
-    os.system("python -m markdown "+doc+ "> README.html")
+    os.system("python -m markdown "+doc + "> README.html")
 
 # Copy all the exe's into one dir - we will use the relaykeysd directory for this
 #for item in ["blehid.pyd",r".\dist\relaykeysd-service\relaykeysd-service.exe",r".\dist\relaykeys-cli\relaykeys-cli.exe",r".\dist\relaykeys-cli-win\relaykeys-cli-win.exe",r".\dist\relaykeys-qt\relaykeys-qt.exe"]:
@@ -68,25 +74,25 @@ for doc in ['README.md']:
 
 # Merge all directories
 if os.name == 'nt':
-	for item in [r"dist\relaykeysd-service",r"dist\relaykeys-cli",r"dist\relaykeys-cli-win",r"dist\relaykeys-qt"]:
+	for item in [r"dist\relaykeysd-service", r"dist\relaykeys-cli", r"dist\relaykeys-cli-win", r"dist\relaykeys-qt"]:
 		moveTree(item, r'dist\relaykeysd')
 if os.name == 'posix':
-	for item in [r"dist/relaykeys-cli",r"dist/relaykeys-cli-win",r"dist/relaykeys-qt"]:
+	for item in [r"dist/relaykeys-cli", r"dist/relaykeys-cli-win", r"dist/relaykeys-qt"]:
 		moveTree(item, r'dist/relaykeysd')
 
 # Copy some other stuff to Dist
-for item in ['relaykeys.cfg','logfile.txt','LICENSE','README.html']:
-    if os.name == 'nt':
-        os.system("copy "+item+ " "+r"dist\relaykeysd")
-    if os.name == 'posix':
-        os.system("cp "+item+ " "+r"dist/relaykeysd/")
+for item in ['relaykeys.cfg', 'logfile.txt', 'LICENSE', 'README.html', 'cli_keymaps']:
+    if os.path.isdir(item):
+        shutil.copytree(item, r"dist/relaykeysd/"+item, dirs_exist_ok=True)
+    else:
+        shutil.copy(item, r"dist/relaykeysd")
 
-
-# Run the nsis 
+# Run the nsis
 if os.name == 'nt':
-    subprocess.run([r"C:\Program Files (x86)\NSIS\makensis.exe","build-installer.nsi"])   
+    subprocess.run(
+        [r"C:\Program Files (x86)\NSIS\makensis.exe", "build-installer.nsi"])
 if os.name == 'posix':
-    plist  = """<?xml version="1.0" encoding="UTF-8"?>
+    plist = """<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
@@ -103,11 +109,10 @@ if os.name == 'posix':
 </dict>
 </plist>
     """
-    # Write dir 
+    # Write dir
     # Write the Plist to the dmg
     file = open("dist/relaykeysd/RelayKeys.plist", "w")
     file.write(plist)
     file.close
     # Lets just rename the dir - A user can just dump this in their Applications dirs
     os.system("mv dist/relaykeysd dist/RelayKeys")
-
